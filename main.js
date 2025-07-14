7273292e60d2e0aa7793f0a9d85df7b1fb1b476f
PushkarJagtap <pushkarjagtap32@outlook.com>
2025-07-15 00:27:18 +0530

    added some extra code

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const city = document.getElementById("city-name")
 const inputcity = document.getElementById("cityInput")
+const weather = document.getElementById("currentweather")
+const time = document.getElementById("obseravationTime")
+// const weatherDiscription = document.getElementById("weather_discription")
 
 const apiKey = "8b04fd047fbda4a0a3937eca6c646fbb";
 
@@ -7,11 +10,23 @@ async function getWeatherData(){
     const res = await fetch(`http://api.weatherstack.com/current?access_key=${"8b04fd047fbda4a0a3937eca6c646fbb"}&query=${cityInput.value}`)
     const data = await res.json();
     console.log(data);
+      if (!cityInput.value) {
+        alert("Enter city");
+        return;
+    }
+
     
     const name = data.location.name;
     const region = data.location.region;
     const country = data.location.country;
+    const observationTime = data.current.observation_time;
+    const temperature = data.current.temperature;
+   
 
-    city.innerText = `${name} (${region}) ${country}`
-}
+    city.innerText = `${name} (${region}) ${country}  `
+    weather.innerText = `${temperature}  `
+    time.innerText = `${observationTime}`
+
+
+ }
 
